6a3ee488eea0e3ad6ff874f99ebe5e6b28522666
Gustavo Lopes <gustavo.lopes@gs3tecnologia.com.br>
2023-12-01 18:31:56 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CustomFormModule } from '../../projects/ionic-custom-form/src/lib/custom-form.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CustomFormModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
